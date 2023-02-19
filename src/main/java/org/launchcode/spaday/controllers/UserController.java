da5ba897fd5be47cab13e37cc9c0a8acfdd18853
Eric Drake <eric.drake2011@gmail.com>
2023-02-19 13:07:33 -0600

    served add.html on 'user/add' get route

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,2 +1,16 @@
-package org.launchcode.spaday.controllers;public class UserController {
+package org.launchcode.spaday.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping(value = "/add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
 }
