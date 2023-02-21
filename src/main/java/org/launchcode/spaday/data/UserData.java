4bc52e3d2e00a06b78d0030459cbd7f93ab26903
Eric Drake <eric.drake2011@gmail.com>
2023-02-20 19:24:17 -0600

    added add, getAll, and findById methods

--- src/main/java/org/launchcode/spaday/data/UserData.java
+++ src/main/java/org/launchcode/spaday/data/UserData.java
@@ -1,2 +1,29 @@
-package org.launchcode.spaday.data;public class UserData {
+package org.launchcode.spaday.data;
+
+import org.launchcode.spaday.models.User;
+
+import java.util.ArrayList;
+
+public class UserData {
+    private ArrayList<User> userList = new ArrayList<>();
+
+    public ArrayList<User> getAll() {
+        return userList;
+    }
+
+    public void add(User user) {
+        userList.add(user);
+    }
+
+    public User getById(int id) {
+
+        for ( User user : userList) {
+            if (user.getId() == id) {
+
+                return user;
+            }
+        }
+
+        return null;
+    }
 }
